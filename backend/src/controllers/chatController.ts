725670a9f3ab3b2073e9b28fe349525c109b633b
Khải Béo <nguyenkhai131104@gmail.com>
2026-02-06 18:19:20 +0700

    Implement chat, message, and user controllers with routes; add authentication middleware for secure access

---
+++ backend/src/controllers/chatController.ts
@@ -0,0 +1,60 @@
+import type { NextFunction, Response } from "express";
+import type { AuthRequest } from "../middleware/auth";
+import { Chat } from "../models/Chat";
+
+export async function getChats(req: AuthRequest, res: Response, next:NextFunction) {
+    try {
+        const userId = req.userId;
+
+        const chats = await Chat.find({ participants: userId }).populate("participants", "name email avatar").populate("latestMessage").sort({ lastMessageAt: -1 });
+    
+        const formattedChats = chats.map(chat => { 
+            const otherParticipants = chat.participants.find(participant => participant._id.toString() !== userId);
+
+            return {
+                _id: chat._id,
+                participant: otherParticipants,
+                latestMessage: chat.lastMessage,
+                lastMessageAt: chat.lastMessageAt,
+                createdAt: chat.createdAt,
+            };
+        });
+        
+        res.json({ chats: formattedChats });
+    } catch (error) {
+        res.status(500);
+        next(error);
+    }
+}
+
+export async function getOrCreateChat(req: AuthRequest, res: Response, next:NextFunction) {
+    try {
+        const userId = req.userId;
+        const { participantId } = req.params;
+
+        let chat = await Chat.findOne({
+            participants: { $all: [userId, participantId] },
+        }).populate("participants", "name email avatar").populate("latestMessage");
+
+        if (!chat) {
+            const newChat = new Chat({ participants: [userId, participantId] });
+            await newChat.save();
+            chat = await newChat.populate("participants", "name email avatar");
+        }
+
+        const otherParticipant = chat.participants.find((participant: any) => participant._id.toString() !== userId);
+
+        res.json({
+            chat: {
+                _id: chat._id,
+                participant: otherParticipant ?? null,
+                latestMessage: chat.lastMessage,
+                lastMessageAt: chat.lastMessageAt,
+                createdAt: chat.createdAt,
+            }
+        });
+    } catch (error) {
+        res.status(500);
+        next(error);
+    }
+}

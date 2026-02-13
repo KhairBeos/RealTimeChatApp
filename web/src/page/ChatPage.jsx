d332ce43ef5aa56f554651a18c2ed3e3212b4382
Khải Béo <nguyenkhai131104@gmail.com>
2026-02-13 22:40:27 +0700

    feat: enhance user synchronization logic and improve error handling

--- web/src/page/ChatPage.jsx
+++ web/src/page/ChatPage.jsx
@@ -1,13 +1,13 @@
 import { useAuth } from "@clerk/clerk-react";
 
 function ChatPage() {
-  const {signOut} = useAuth();
+  const { signOut } = useAuth();
   return (
     <div>
       ChatPage
       <button onClick={() => signOut()}>Sign out</button>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default ChatPage
+export default ChatPage;
